02490f6c75e96c2c8a209309167854bea55ae976
nick.z <nick.zozor@onefile.co.uk>
2021-04-14 14:46:37 +0100

    add scroll top header translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'flavia-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'flavia-website';
+  header: any;
+  sticky: any;
+  tempPageYOffset: any;
+
+  @HostListener('window:scroll', ['$event'])
+  private onWindowScroll(event: any): void {
+      console.log(window.pageYOffset);
+      const direction = window.pageYOffset > this.tempPageYOffset ? 'down' : 'up';
+      this.tempPageYOffset = window.pageYOffset;
+      if (!this.header) { return; }
+      if (window.pageYOffset > this.sticky) {
+        this.header.classList.add('shrink');
+      } else {
+        this.header.classList.remove('shrink');
+      }
+      if (direction === 'down' && window.pageYOffset > 30) {
+        this.header.classList.add('hide');
+      } else {
+        this.header.classList.remove('hide');
+      }
+  }
+
+  public ngAfterViewInit(): void {
+    // Get the header
+    this.header = document.getElementById('header');
+    // Get the offset position of the navbar
+    this.sticky = this.header.offsetTop;
+  }
 }
