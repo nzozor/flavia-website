e4a8fdcdbeab0922f4a357d1988a5fe7d1e78406
nick.z <nick.zozor@onefile.co.uk>
2021-04-20 16:04:20 +0100

    add new logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    screens: {
-      'mobile': '640px',
-      // => @media (min-width: 640px) { ... }
+    // screens: {
+    //   'mobile': '640px',
+    //   // => @media (min-width: 640px) { ... }
       
-      'tablet': '768px',
-      // => @media (min-width: 640px) { ... }
+    //   'tablet': '768px',
+    //   // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
+    //   'laptop': '1024px',
+    //   // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
+    //   'desktop': '1280px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
   },
   variants: {
     extend: {},
