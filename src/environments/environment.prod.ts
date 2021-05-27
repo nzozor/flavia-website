bb5cbd4b0739441d4f1445efc85b1598962774b8
nick.z <nick.zozor@onefile.co.uk>
2021-05-27 19:09:39 +0100

    htpps cms.tracks..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   jwt: '8cdb3c62-1c40-412a-a111-5b438959725d',
-  cmsUrl: 'http://167.99.200.71:1338'
+  cmsUrl: 'https://cms.tracksdigital.com'
 };
