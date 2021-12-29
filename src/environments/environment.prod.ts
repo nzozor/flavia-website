24143256d061b65b9255d650b31ab04c24133566
Nick Zozor <nick.zozor@gmail.com>
2021-12-29 16:32:59 +0100

    add new cms domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   jwt: '8cdb3c62-1c40-412a-a111-5b438959725d',
-  cmsUrl: 'https://cms.tracksdigital.com'
+  cmsUrl: 'https://cms.flaviafrigeri.com'
 };
